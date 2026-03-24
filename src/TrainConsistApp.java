9127e4d7a310b7ad00a03d578df4146402f42447
PragalbhRai <pragalbhrai@gmail.com>
2026-03-24 09:07:50 +0530

    UC6: Map bogie to capacity using HashMap

--- src/TrainConsistApp.java
+++ src/TrainConsistApp.java
@@ -5,25 +5,25 @@ public class TrainConsistApp {
   public static void main(String[] args) {
 
     System.out.println("======================================");
-    System.out.println("UC5 - Preserve Insertion Order of Bogies");
+    System.out.println("UC6 - Map Bogie to Capacity (HashMap)");
     System.out.println("======================================\n");
 
-    // Create LinkedHashSet
-    Set<String> trainFormation = new LinkedHashSet<>();
+    // Create HashMap
+    Map<String, Integer> bogieCapacity = new HashMap<>();
 
-    // Add bogies
-    trainFormation.add("Engine");
-    trainFormation.add("Sleeper");
-    trainFormation.add("Cargo");
-    trainFormation.add("Guard");
+    // Insert data
+    bogieCapacity.put("Sleeper", 72);
+    bogieCapacity.put("AC Chair", 56);
+    bogieCapacity.put("First Class", 24);
+    bogieCapacity.put("Cargo", 120);
 
-    // Add duplicate
-    trainFormation.add("Sleeper"); // duplicate (ignored)
+    // Display data
+    System.out.println("Bogie Capacity Details:\n");
 
-    // Display formation
-    System.out.println("Final Train Formation:");
-    System.out.println(trainFormation);
+    for (Map.Entry<String, Integer> entry : bogieCapacity.entrySet()) {
+      System.out.println(entry.getKey() + " -> " + entry.getValue());
+    }
 
-    System.out.println("\nUC5 operations completed successfully...");
+    System.out.println("\nUC6 bogie-capacity mapping completed...");
   }
 }
