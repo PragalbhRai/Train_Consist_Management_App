27be6c8f5acc66fc4267bef24442d2fd1466d9ec
PragalbhRai <pragalbhrai@gmail.com>
2026-04-09 08:52:46 +0530

    UC17: Sort bogie names using Arrays.sort()

--- src/TrainConsistApp.java
+++ src/TrainConsistApp.java
@@ -1,48 +1,33 @@
+import java.util.Arrays;
+
 public class TrainConsistApp {
 
   public static void main(String[] args) {
 
     System.out.println("======================================");
-    System.out.println("UC16 - Sort Passenger Bogies (Bubble Sort)");
+    System.out.println("UC17 - Sort Bogie Names (Arrays.sort)");
     System.out.println("======================================\n");
 
     // INPUT ARRAY
-    int[] capacities = {72, 56, 24, 70, 60};
+    String[] bogieNames = {
+            "Sleeper",
+            "AC Chair",
+            "First Class",
+            "General",
+            "Luxury"
+    };
 
-    System.out.print("Before Sorting: ");
-    printArray(capacities);
+    System.out.println("Before Sorting:");
+    System.out.println(Arrays.toString(bogieNames));
 
     // ================================
-    // BUBBLE SORT LOGIC
+    // BUILT-IN SORT
     // ================================
-    int n = capacities.length;
+    Arrays.sort(bogieNames);
 
-    for (int i = 0; i < n - 1; i++) {
+    System.out.println("\nAfter Sorting:");
+    System.out.println(Arrays.toString(bogieNames));
 
-      for (int j = 0; j < n - i - 1; j++) {
-
-        // Compare adjacent elements
-        if (capacities[j] > capacities[j + 1]) {
-
-          // Swap
-          int temp = capacities[j];
-          capacities[j] = capacities[j + 1];
-          capacities[j + 1] = temp;
-        }
-      }
-    }
-
-    System.out.print("\nAfter Sorting: ");
-    printArray(capacities);
-
-    System.out.println("\n\nUC16 bubble sort completed...");
-  }
-
-  // Helper Method
-  public static void printArray(int[] arr) {
-    for (int num : arr) {
-      System.out.print(num + " ");
-    }
-    System.out.println();
+    System.out.println("\nUC17 sorting completed...");
   }
 }
